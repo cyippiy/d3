8d6d07d21b3697a3a1d3c0ee3bcbf17b5f8e3f66
Chris Yip <chris.yip87@gmail.com>
2019-04-25 14:25:58 -0700

    starbreak coffee start

--- 03/3.13.0/js/main.js
+++ 03/3.13.0/js/main.js
@@ -4,3 +4,15 @@
 *    Project 1 - Star Break Coffee
 */
 
+let svg = d3.select("#chart-area")
+    .append("svg")
+        .attr("width", "800")
+        .attr("height", "800");
+
+d3.json("data/revenues.json").then( (data) =>{
+    data.forEach( (d) => {
+        d.revenue = +d.revenue;
+        d.profit = +d.profit;
+    });
+    console.log(data);
+})
