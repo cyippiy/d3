79fe095a048faf361a91109459b64c8bdef76dc1
Chris Yip <chris.yip87@gmail.com>
2019-07-02 17:17:35 -0700

    project 2 wip

--- 05/5.10.0/js/main.js
+++ 05/5.10.0/js/main.js
@@ -3,7 +3,23 @@
 *    Mastering Data Visualization with D3.js
 *    Project 2 - Gapminder Clone
 */
+const margin = { left: 80, right: 20, top: 50, bottom: 100 };
+const height = 500 - margin.top - margin.bottom,
+	width = 800 - margin.left - margin.right;
+
+
+const g = d3.select("#chart-area")
+	.append("svg")
+		.attr("width",width + margin.left + margin.right)
+		.attr("height",height + margin.top + margin.bottom)
+	.append("g")
+		.attr("transform",`translate(${margin.left},${margin.top})`);
 
 d3.json("data/data.json").then(function(data){
 	console.log(data);
+	data.forEach();
+
+	let y = d3.scaleLinear()
+		.domain([0,90])
+		.range();
 })
